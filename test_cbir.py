a5ca5c61f1ca547a40ab683ecbed40368f24e4ea
cheng ying <chengying@tezign.com>
2020-03-05 18:06:12 +0800

    test rerank her

--- test_cbir.py
+++ test_cbir.py
@@ -45,12 +45,13 @@ def get_feature(args, image_path):
 
 
 if __name__ == '__main__':
-    args = AttrDict({"model": "attention", "pca": False, "multi_scale": False, 'id': "10",'rerank':"none"})
-    feature2 = get_feature(args, "../bgy_test/2/137-2.jpg")
-    feature1 = get_feature(args, "../bgy_test/1/137-1.jpg")
-    print(np.dot(feature2[0], feature1[0]))
-    print("===feature1====")
-    print(feature1)
-    model = get_model(args.model).to(device)
-    search = Search(model, "10vectors_.pkl", "none", device, args)
-    print(search.search("../bgy_test/1/137-1.jpg", 10))
+    # args = AttrDict({"model": "attention", "pca": False, "multi_scale": False, 'id': "10",'rerank':"none"})
+    # feature2 = get_feature(args, "../bgy_test/2/137-2.jpg")
+    # feature1 = get_feature(args, "../bgy_test/1/137-1.jpg")
+    # print(np.dot(feature2[0], feature1[0]))
+    # print("===feature1====")
+    # print(feature1)
+    # model = get_model(args.model).to(device)
+    # search = Search(model, "10vectors_.pkl", "none", device, args)
+    # print(search.search("../bgy_test/1/137-1.jpg", 10))
+    res_show("data/10query_res.pkl")
